6fdf2ab62086c1b2699c83b3df281532cb99400d
Scott Baker <smbaker@gmail.com>
2019-02-09 20:13:48 -0800

    crontab notes; fan rpm config

--- start_envmond.sh
+++ start_envmond.sh
@@ -5,14 +5,21 @@ if [ ! -f /etc/envmon/station ]; then
     exit 1
 fi
 
+FANPWM=200
+if [ -f /etc/envmon/config ]; then
+    source /etc/envmon/config
+fi
+
 if [[ $EUID -ne 0 ]]; then
     echo "This script must be run as root"
     exit 1
 fi
 
-pigpiod -s 2 || echo "failed to install pgpiod -- already installed?"
+pkill -f envmond.py
+
+/usr/local/bin/pigpiod -s 2 || echo "failed to install pgpiod -- already installed?"
 
 cd /home/pi/envmon
 STATION=`cat /etc/envmon/station`
 
-su - pi -c "bash -c \"cd /home/pi/envmon && nohup python /home/pi/envmon/envmond.py -S $STATION -v --rrd --udp 198.0.0.55:1234 --prometheus 8000 -F 200 >> /tmp/envmond.log 2>&1 &\""
+su - pi -c "bash -c \"cd /home/pi/envmon && nohup python /home/pi/envmon/envmond.py -S $STATION -v --rrd --udp 198.0.0.55:1234 --prometheus 8000 -F $FANPWM >> /tmp/envmond.log 2>&1 &\""
